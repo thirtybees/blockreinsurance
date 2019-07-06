e4e5d5d1533e89528d7e0dc9a6c6bf243b6b8e91
Markus Hitter <mah@jump-ing.de>
2019-07-06 13:51:01 +0200

    Make release 3.0.3.

--- blockreinsurance.php
+++ blockreinsurance.php
@@ -34,7 +34,7 @@ class Blockreinsurance extends Module
 	{
 		$this->name = 'blockreinsurance';
 		$this->tab = 'front_office_features';
-		$this->version = '3.0.2';
+		$this->version = '3.0.3';
 		$this->author = 'thirty bees';
 		$this->need_instance = 0;
 
